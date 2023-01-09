6ca701d3c41b0c75e4fbbbae5453dfcc4567c9f0
Phung Gia Bao <bao.future94@gmail.com>
2023-01-09 16:58:56 +0700

    updater

---
+++ updater.js
@@ -0,0 +1,33 @@
+// Electron-updater Module
+const { dialog } = require("electron");
+const { autoUpdater } = require("electron-updater");
+
+// Configure log debugging
+autoUpdater.logger = require("electron-log");
+autoUpdater.logger.transports.file.level = "info";
+
+// Disable auto downloading of updates
+autoUpdater.autoDownload = false;
+
+// Singe export to check for and apply any available updates
+module.exports = () => {
+  // Check for update (GH Releases)
+  console.log("Checking for updates");
+  autoUpdater.checkForUpdates();
+
+  // Listen for update found
+  autoUpdater.on("update-available", () => {
+    // Promt user to start download
+    dialog.showMessageBox({
+        type: 'info',
+        title: 'Update available',
+        message: 'A new version of Readit is available. Do you want to update now?',
+        buttons: ['Update', 'No']
+    }).then(result => {
+        let buttonIndex = result.response
+
+        // If button 0 (update), start downloading the update
+        if(buttonIndex === 0) autoUpdater.downloadUpdate()
+    })
+  });
+};
