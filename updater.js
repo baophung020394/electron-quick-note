172d3cda865dc2b493b03c40fee1fa5161f54937
Phung Gia Bao <bao.future94@gmail.com>
2023-01-10 10:38:04 +0700

    test 2

--- updater.js
+++ updater.js
@@ -7,27 +7,26 @@ autoUpdater.logger = require("electron-log");
 autoUpdater.logger.transports.file.level = "info";
 
 // Disable auto downloading of updates
-autoUpdater.autoDownload = false;
+// autoUpdater.autoDownload = false;
 
 // Singe export to check for and apply any available updates
 module.exports = () => {
   // Check for update (GH Releases)
-  console.log("Checking for updates");
   autoUpdater.checkForUpdates();
 
   // Listen for update found
-  autoUpdater.on("update-available", () => {
-    // Promt user to start download
-    dialog.showMessageBox({
-        type: 'info',
-        title: 'Update available',
-        message: 'A new version of Readit is available. Do you want to update now?',
-        buttons: ['Update', 'No']
-    }).then(result => {
-        let buttonIndex = result.response
+//   autoUpdater.on("update-available", () => {
+//     // Promt user to start download
+//     dialog.showMessageBox({
+//         type: 'info',
+//         title: 'Update available',
+//         message: 'A new version of Readit is available. Do you want to update now?',
+//         buttons: ['Update', 'No']
+//     }).then(result => {
+//         let buttonIndex = result.response
 
-        // If button 0 (update), start downloading the update
-        if(buttonIndex === 0) autoUpdater.downloadUpdate()
-    })
-  });
+//         // If button 0 (update), start downloading the update
+//         if(buttonIndex === 0) autoUpdater.downloadUpdate()
+//     })
+//   });
 };
